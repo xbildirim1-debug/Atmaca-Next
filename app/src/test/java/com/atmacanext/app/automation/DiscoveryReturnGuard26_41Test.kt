936caf8f48deb64503eede9ac56d54faf0bf7723
xbildirim1-debug <xbildirim1@gmail.com>
2026-09-10 20:52:22 +0300

    26.41: hedef profile dönüş Back kilidi testini ekle

---
+++ app/src/test/java/com/atmacanext/app/automation/DiscoveryReturnGuard26_41Test.kt
@@ -0,0 +1,34 @@
+package com.atmacanext.app.automation
+
+import android.graphics.Rect
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class DiscoveryReturnGuard26_41Test {
+    private fun backNode() = NodeSnapshot(
+        text = null,
+        contentDescription = "Geri",
+        viewId = "com.twitter.android:id/back",
+        className = "android.widget.ImageButton",
+        clickable = true,
+        enabled = true,
+        bounds = Rect(20, 30, 100, 110),
+        visible = true,
+    )
+
+    @Test fun returnToDiscoveryTargetNeverBacksOutOfProfile() {
+        val nodes = listOf(backNode())
+        assertNull(NavigationSurfaceEvidence.backIndex(nodes, XFlowStage.RETURN_DISCOVERY_TARGET, XScreen.PROFILE))
+    }
+
+    @Test fun returnRouteStillAllowsBackFromTweetDetail() {
+        val nodes = listOf(backNode())
+        assertEquals(0, NavigationSurfaceEvidence.backIndex(nodes, XFlowStage.RETURN_DISCOVERY_TARGET, XScreen.TWEET_DETAIL))
+    }
+
+    @Test fun unrelatedProfileFlowsKeepNormalBackBehavior() {
+        val nodes = listOf(backNode())
+        assertEquals(0, NavigationSurfaceEvidence.backIndex(nodes, XFlowStage.OPEN_SOURCE_PROFILE, XScreen.PROFILE))
+    }
+}
