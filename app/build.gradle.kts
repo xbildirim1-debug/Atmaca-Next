aed2980b10a5759930f697929ad73ea0bef7b1d9
xbildirim1-debug <xbildirim1@gmail.com>
2026-09-10 15:40:05 +0300

    26.36: bump version for engagement fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.atmacanext.v258"
         minSdk = 26
         targetSdk = 36
-        versionCode = 83
-        versionName = "26.35-comment-thread-scroll"
+        versionCode = 84
+        versionName = "26.36-engagement-drain"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
